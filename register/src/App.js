a956b6fb859a5bf97e34ffd6a44eadc711f44b73
Betulcode <helen.kut90@gmail.com>
2020-11-29 05:37:19 +0300

    Navbar is added

--- register/src/App.js
+++ register/src/App.js
@@ -1,9 +1,11 @@
 import {SignUp} from "./pages/Signup"
+import Navbar from "./components/navbar"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
      <SignUp/>
     </div>
   );
